8a937bcac358fba382e614c12afd76c21cb37d24
gmkseta <gmkseta@gmail.com>
2019-06-05 03:36:02 +0900

    이거랃 ㅜ

--- crawler/Twitter/crawler/Crawler.py
+++ crawler/Twitter/crawler/Crawler.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-import os
-import sys
-sys.path.append(os.path("/home/ubuntu/.local/lib/python3.5/site-packages"))
-import time
 import sns_data
 
 
 if __name__ == "__main__":
     try:
-        
-        start_time = time.time()
-        browser = sns_data.CrawlBrowser()
-        
+        browser = sns_data.CrawlBrowser()        
         browser.crawl_data()
         
     except:
