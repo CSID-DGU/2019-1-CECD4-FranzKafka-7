5deb4d660c222f8386dff44527af2f93cdf66303
gmkseta <gmkseta@gmail.com>
2019-06-07 16:30:56 +0900

    bulk insert

--- crawler/Twitter/admin-server/app/models/twitter.rb
+++ crawler/Twitter/admin-server/app/models/twitter.rb
@@ -1,14 +1,18 @@
 class Twitter < ApplicationRecord
     belongs_to :keyword
     validates_uniqueness_of :item_id
-
+    require 'activerecord-import/active_record/adapters/mysql2_adapter'
     def self.json_insert(file, keyword_id)
         p 'start insert'
+        
         old_logger = ActiveRecord::Base.logger
         ActiveRecord::Base.logger = nil
-
+        
         json = JSON.parse(file)
-        json.each do |j|
+        size = json.size
+        tweets = []
+
+        json.each_with_index do |j,i|
             
             j.delete("html")
             j.delete("url")
@@ -18,16 +22,19 @@ class Twitter < ApplicationRecord
             j["name"] = j.delete("user")
             j["keyword_id"] = keyword_id
             j["p_date"] = p_date
-            t = Twitter.new(j)
-            begin
-                t.save!
-            rescue ActiveRecord::RecordInvalid => e
-                next
+            tweets << Twitter.new(j)
+            
+            if i%1000 == 0 or i == size-1
+                begin
+                    Twitter.import tweets, validate_uniqueness: true
+                    tweets = []
+                rescue Exception => e
+                    p e.message
+                    next
+                end
             end
-
-        ActiveRecord::Base.logger = old_logger
-
-
+            
         end
+        ActiveRecord::Base.logger = old_logger
     end
 end
