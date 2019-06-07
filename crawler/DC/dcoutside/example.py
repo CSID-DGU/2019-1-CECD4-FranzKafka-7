1cf7fcf3da77ee329dc1082bd1883dca5a5e0abe
gmkseta <gmkseta@gmail.com>
2019-06-08 03:22:16 +0900

    argv

--- crawler/DC/dcoutside/example.py
+++ crawler/DC/dcoutside/example.py
@@ -2,24 +2,28 @@ from dcoutside.crawler import DCInsideCrawler
 from pprint import pprint
 import json
 import time
+import sys
 crawler = DCInsideCrawler()
 
 
 
 
 to_json = []
-i = 2
+i = int(sys.argv[1])
+end = int(sys.argv[2])
 from_i = i
 while(True):
     try:
 
         to_json.append(pprint(crawler.get_post('jijinhee', i)))
-        time.sleep(3)
-        if i%100 == 0 :
+        time.sleep(6)
+        if i%100 == 99 or i == end :
             with open(str(from_i)+'_'+str(i)+'.json', 'w') as outfile:
                 json.dump(to_json, outfile)
             to_json = []
             from_i = i+1
+        if i == end:
+            break;
         i+=1
     except Exception as e:
         print(e)
