a8b1e7a6e281b0deb20f4c9804b74f1ea9e55ad0
gmkseta <gmkseta@gmail.com>
2019-06-08 13:57:57 +0900

    add keyboard interrupt

--- crawler/DC/dcoutside/example.py
+++ crawler/DC/dcoutside/example.py
@@ -15,7 +15,7 @@ from_i = i
 while(True):
     try:
         time.sleep(3)
-        if len(t_json) >= 100 or i == end :
+        if len(to_json) >= 100 or i == end :
             with open(str(from_i)+'_'+str(i-1)+'.json', 'w') as outfile:
                 json.dump(to_json, outfile)
             to_json = []
@@ -29,6 +29,12 @@ while(True):
             break;
 
         i+=1
+    except KeyboardInterrupt:
+        print('keyboard interrupt')
+        with open(str(from_i)+'_'+str(i-1)+'.json', 'w') as outfile:
+            json.dump(to_json, outfile)
+            
+            
     except Exception as e:
         print(e)
         i+=1
