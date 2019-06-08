ac0bce152e08f4bc449e60bc3fd3dd3d3cf4d6a8
gmkseta <gmkseta@gmail.com>
2019-06-08 18:03:31 +0900

    move admin

--- crawler/Twitter/admin-server/db/migrate/20190607043534_convert_charsetto_utf8mb4.rb
+++
@@ -1,5 +0,0 @@
-class ConvertCharsettoUtf8mb4 < ActiveRecord::Migration[5.2]
-  def change
-    execute("ALTER TABLE twitters CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
-  end
-end
--- crawler/Twitter/admin-server/db/migrate/20190607043904_add_keyword_to_twitters.rb
+++
@@ -1,5 +0,0 @@
-class AddKeywordToTwitters < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :twitters, :keyword, foreign_key: true
-  end
-end
